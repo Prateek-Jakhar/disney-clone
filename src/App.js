b8f3272c40071bb9bc3e3d5a762c82148a3e0fe0
prateek <prateekjakhar123@gmail.com>
2023-01-18 17:30:35 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
